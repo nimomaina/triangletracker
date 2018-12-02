7b4f6ee78574b0857770adb0b6a08b9cdcd2bc21
nimoken <nimomaina.nm@gmail.com>
2018-12-02 22:06:57 +0300

    Creating a function in js file

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function triangletracker(){
+  var side1 = parseInt(document.getElementById('a').value);
+  var side2 = parseInt(document.getElementById('b').value);
+  var side3 = parseInt(document.getElementById('c').value);
+  if(a + b < c && b + c < a && a + c < b){
+    if(a == b && b == c && a == c){
+
+    }
+  }else{
+
+  }//end of parent if
+}
