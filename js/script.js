28de2e7b486d81f1f2dbd72ddf3c58c883b73f9a
nimoken <nimomaina.nm@gmail.com>
2018-12-02 22:43:44 +0300

    Correcting js function

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-function triangletracker(){
-  var side1 = parseInt(document.getElementById('a').value);
-  var side2 = parseInt(document.getElementById('b').value);
-  var side3 = parseInt(document.getElementById('c').value);
+function triangletracker() {
+  var a = parseInt(document.getElementById("side1").value);
+  var b = parseInt(document.getElementById("side2").value);
+  var c = parseInt(document.getElementById("side3").value);
 
 
-  if((a + b > c) && (b + c > a) && (a + c > b)){
+  if(a + b > c && b + c > a && a + c > b){
     if(a == b && b == c && a == c){
-      document.getElementById('output').value = 'This is an equilateral triangle';
+      document.getElementById("output").value = "This is an equilateral triangle";
     }//end of equilateral if
     else if (a == b || b == c || a == c) {
-      document.getElementById('output').value = 'This is an Isosceles triangle';
+      document.getElementById("output").value = "This is an Isosceles triangle";
     }//end of isosceles if
     else {
-      document.getElementById('output').value = 'This is a Scalene triangle';
+      document.getElementById("output").value = "This is a Scalene triangle";
     }
   }
   else{
-    document.getElementById('output').value = 'This is not a triangle';
+    document.getElementById("output").value = "This is not a triangle";
 
   }//end of parent if
-}
+}//end of function
