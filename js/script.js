f10cf781c23ab18f54fe8c706aaf2e1d8b0cda2f
nimoken <nimomaina.nm@gmail.com>
2018-12-02 22:15:25 +0300

    Adding a nested if statement to find the type of triangle

--- js/script.js
+++ js/script.js
@@ -2,11 +2,21 @@ function triangletracker(){
   var side1 = parseInt(document.getElementById('a').value);
   var side2 = parseInt(document.getElementById('b').value);
   var side3 = parseInt(document.getElementById('c').value);
-  if(a + b < c && b + c < a && a + c < b){
-    if(a == b && b == c && a == c){
 
+
+  if((a + b > c) && (b + c > a) && (a + c > b)){
+    if(a == b && b == c && a == c){
+      document.getElementById('output').value = 'This is an equilateral triangle';
+    }//end of equilateral if
+    else if (a == b || b == c || a == c) {
+      document.getElementById('output').value = 'This is an Isosceles triangle';
+    }//end of isosceles if
+    else {
+      document.getElementById('output').value = 'This is a Scalene triangle';
     }
-  }else{
+  }
+  else{
+    document.getElementById('output').value = 'This is not a triangle';
 
   }//end of parent if
 }
