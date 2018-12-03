08e03110fe993022cc9bd43dbd30833866a4af22
nimoken <nimomaina.nm@gmail.com>
2018-12-03 11:55:24 +0300

    adding css in input tags

--- js/script.js
+++ js/script.js
@@ -3,22 +3,22 @@ function triangletracker() {
   var b = parseInt(document.getElementById("side2").value);
   var c = parseInt(document.getElementById("side3").value);
 
-  var type = ['Equilateral', 'Isosceles', 'Scalene', 'Not a triangle']
+  var type = ['Equilateral triangle', 'Isosceles triangle', 'Scalene triangle', 'Not a triangle']
 
 
   if(a + b > c && b + c > a && a + c > b){
     if(a == b && b == c && a == c){
-      document.getElementById("output").innerHTML = type[0];
+      document.getElementById("output").innerHTML = 'This is an' + type[0];
     }//end of equilateral if
     else if (a == b || b == c || a == c) {
-      document.getElementById("output").innerHTML = type[1];
+      document.getElementById("output").innerHTML = 'This is an' + type[1];
     }//end of isosceles if
     else {
-      document.getElementById("output").innerHTML = type[2];
+      document.getElementById("output").innerHTML ='This is a' +  type[2];
     }
   }
   else{
-    document.getElementById("output").innerHTML = type[3];
+    document.getElementById("output").innerHTML = 'Sorry this is' + type[3];
 
   }//end of parent if
 }//end of function
